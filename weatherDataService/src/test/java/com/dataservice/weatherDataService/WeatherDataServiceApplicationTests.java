a1ad3f706e2cb1dcac1ccebd2b7c567cb82a8156
andreasbendorio <andreasbendorio6@gmail.com>
2024-05-05 17:10:54 +0200

    changed proj structure

---
+++ weatherDataService/src/test/java/com/dataservice/weatherDataService/WeatherDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataservice.weatherDataService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
